2562cf8db11858b0239252fbd8673cfbe7c5aaf0
Andres Lemos <Lemos_1325@hotmail.com>
2023-02-13 13:21:45 -0500

    se crean lo modelos de la aplicacion

---
+++ src/main/java/org/andlemro/test/sprintboot/app/springboot_test/models/Banco.java
@@ -0,0 +1,45 @@
+package org.andlemro.test.sprintboot.app.springboot_test.models;
+
+public class Banco {
+	
+	private Long id;
+	private String nombre;
+	private int totalTransferencias;
+	
+	public Banco() {
+		super();
+	}
+	
+	public Banco(Long id, String nombre, int totalTransferencias) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.totalTransferencias = totalTransferencias;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getTotalTransferencias() {
+		return totalTransferencias;
+	}
+
+	public void setTotalTransferencias(int totalTransferencias) {
+		this.totalTransferencias = totalTransferencias;
+	}
+
+
+}
---
+++ src/main/java/org/andlemro/test/sprintboot/app/springboot_test/models/Cuenta.java
@@ -0,0 +1,75 @@
+package org.andlemro.test.sprintboot.app.springboot_test.models;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import org.andlemro.test.sprintboot.app.springboot_test.esceptions.DineroInsuficienteException;
+
+public class Cuenta {
+
+	private Long id;
+	private String persona;
+	private BigDecimal saldo;
+
+	public Cuenta() {
+		super();
+	}
+
+	public Cuenta(Long id, String persona, BigDecimal saldo) {
+		super();
+		this.id = id;
+		this.persona = persona;
+		this.saldo = saldo;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPersona() {
+		return persona;
+	}
+	public void setPersona(String persona) {
+		this.persona = persona;
+	}
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cuenta other = (Cuenta) obj;
+		return Objects.equals(id, other.id) && Objects.equals(persona, other.persona)
+				&& Objects.equals(saldo, other.saldo);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, persona, saldo);
+	}
+	
+	public void debito(BigDecimal monto) {
+		BigDecimal nuevoSaldo = this.saldo.subtract(monto);
+		if(nuevoSaldo.compareTo(BigDecimal.ZERO) < 0) {
+			throw new DineroInsuficienteException("Dinero insuficiente en la cuenta");
+		}
+		this.saldo = nuevoSaldo;
+	}
+	
+	public void credito(BigDecimal monto) {
+		this.saldo = saldo.add(monto);
+	}
+	
+	
+}
