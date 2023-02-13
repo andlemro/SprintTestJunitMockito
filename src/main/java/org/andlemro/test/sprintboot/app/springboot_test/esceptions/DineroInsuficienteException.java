c7e8e5643e5418ce1704901adbef6a3535ef2a83
Andres Lemos <Lemos_1325@hotmail.com>
2023-02-13 13:23:29 -0500

    Se crea excepcion personalizada

---
+++ src/main/java/org/andlemro/test/sprintboot/app/springboot_test/esceptions/DineroInsuficienteException.java
@@ -0,0 +1,7 @@
+package org.andlemro.test.sprintboot.app.springboot_test.esceptions;
+
+public class DineroInsuficienteException extends RuntimeException {
+	public DineroInsuficienteException(String message) {
+		super(message);
+	}
+}
